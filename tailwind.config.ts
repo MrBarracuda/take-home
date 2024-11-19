979cbaf65dc14df0a7e96e2034fb79760134be6d
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2024-11-19 12:53:56 -0500

    feat: add animation and its keyframes to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        appear: {
+          from: {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
+          },
+        },
+        slide: {
+          from: { transform: "translateX(100%)" },
+          to: { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        appear: "appear 1s ease-in-out",
+        slide: "slide 750ms ease-in-out",
+      },
+    },
   },
   plugins: [],
 };
